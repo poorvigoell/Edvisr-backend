8c8140f3e00616c34cefd3aa7b6b54bd6ed0fad8
Poorvi Goel <poorvig45@gmail.com>
2025-04-10 20:07:17 +0530

    cors fix

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://edvisr.netlify.app/"],  # 👈 Allow all origins (frontend domains)
+    allow_origins=["https://edvisr.netlify.app/",
+                   "http://localhost:3000"],  # 👈 Allow all origins (frontend domains)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
